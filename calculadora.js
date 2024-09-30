e1846e35f46a50e46de2389098249cc9072abeda
Chapichiro <zx21student119@colegiovalledelmiro.es>
2024-09-30 09:36:05 +0200

    Dividir dos numeros

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,16 @@ function restar (a,b){
 }
 
 console.log(restar(1,1));
+
+function multi (a,b){
+    return a * b;
+}
+console.log(multi(10,1));
+
+function divi (a,b){
+    if (b  == 0){
+    return "ERROR: no se puede dividir entre 0";
+    }else{
+        return a / b;
+    }
+}
